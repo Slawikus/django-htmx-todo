cdacec9e2925bbb0b980a22337c23cfbde7a14ff
Slawa Gladkov <Slawikus@users.noreply.github.com>
2025-01-12 18:46:59 +0100

    Mark todo portion of the template as a partial

--- configuration/settings.py
+++ configuration/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # core
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party
+    'template_partials',
+    # internal
     'users',
     'todos',
 ]
