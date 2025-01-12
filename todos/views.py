ef75d60f2ada5b64e5f930f4bbea108e4be4d073
Slawa Gladkov <Slawikus@users.noreply.github.com>
2025-01-12 18:39:28 +0100

    List todos

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 
-# Create your views here.
+from todos.models import Todo
+
+
+class ListTodosView(ListView, LoginRequiredMixin):
+    model = Todo
+    template_name = 'todos/list.html'
+    context_object_name = 'todos'
+
+    def get_queryset(self):
+        return self.request.user.todos.order_by('created_at').all()
